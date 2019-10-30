5ea41935653cd9f5bca4fecc04b7e1b39f160017
christopherpryer <christophpryer@gmail.com>
2019-10-30 10:27:43 -0400

    added initial single-page app codebase iteration

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from config import Config
+
+bootstrap = Bootstrap()
+moment = Moment()
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp)
+
+    return app
