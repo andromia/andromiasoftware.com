853bbda90d2a86f3b2383625748665970df6b8d8
Chris Pryer <christophpryer@gmail.com>
2019-11-22 12:54:12 -0500

    added staff login

--- app/__init__.py
+++ app/__init__.py
@@ -21,11 +21,15 @@ def create_app(config_class=Config):
 
     db.init_app(app)
     migrate.init_app(app, db)
+    login.init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
+
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
