3c9c65ca1f36c664bec6e6be26f6a6a49907f836
Chris Pryer <christophpryer@gmail.com>
2019-11-22 11:40:02 -0500

    started routing MDS & TDR pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,3 +5,11 @@ from app.main import bp
 @bp.route('/index', methods=['GET', 'POST'])
 def index():
     return render_template('index.html', title='Home')
+
+@bp.route('/MDS', methods=['GET', 'POST'])
+def MDS():
+    return render_template('MDS.html', title='Model Design Studio')
+
+@bp.route('/TDR', methods=['GET', 'POST'])
+def TDR():
+    return render_template('TDR.html', title='The Duel: Reloaded')
